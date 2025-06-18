c3203c268ffbdf968f26748d2c484d3ea1c25685
beamx999 <thanakitsupanya@gmail.com>
2025-06-18 10:39:33 +0700

    V.3

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-    <h1>Mr.Thaakit Supanya</h1>
+    <center><h1>Mr.Thaakit Supanya</h1></center>
     </>
   );
 }
